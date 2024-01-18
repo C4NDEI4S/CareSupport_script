835e12d423c5fc4d36cd7eec7a2a35a744e08077
C4NDEI4S <156951281+C4NDEI4S@users.noreply.github.com>
2024-01-18 09:21:19 +0000

    Update CareSupport.py

--- CareSupport.py
+++ CareSupport.py
@@ -1,18 +1,31 @@
 from github import Github
+import base64
 
 def upload_to_github(username, password, repo_name, file_path, commit_message):
     # Autenticação no GitHub
     g = Github(username, password)
 
-    # Obtém o repositório desejado (substitua 'your-username' pelo seu nome de usuário e 'your-repo' pelo nome do repositório)
+    # Obtém o repositório desejado
     repo = g.get_user().get_repo(repo_name)
 
-    # Leitura do conteúdo do arquivo
-    with open(file_path, 'r') as file:
-        file_content = file.read()
+    # Obtém o conteúdo atual do arquivo no repositório
+    try:
+        contents = repo.get_contents(file_path)
+        current_content = base64.b64decode(contents.content).decode("utf-8")
+    except Exception:
+        current_content = ""
 
-    # Cria um novo arquivo no repositório do GitHub
-    repo.create_file(file_path, commit_message, file_content)
+    # Leitura do conteúdo do arquivo local
+    with open(file_path, 'r') as file:
+        new_content = file.read()
+
+    # Verifica se o conteúdo é diferente antes de fazer upload
+    if new_content != current_content:
+        # Faz o upload do novo conteúdo
+        repo.update_file(file_path, commit_message, new_content, contents.sha)
+        print("Arquivo atualizado no repositório do GitHub.")
+    else:
+        print("O conteúdo do arquivo não foi alterado. Nenhum upload necessário.")
 
 if __name__ == "__main__":
     # Substitua as informações abaixo com suas credenciais e detalhes do arquivo
@@ -20,6 +33,6 @@ if __name__ == "__main__":
     github_password = "sua-senha-github"
     repository_name = "nome-do-repositorio"
     local_file_path = "caminho/do/arquivo/no/seu/computador/arquivo.txt"
-    commit_msg = "Adicionando arquivo ao repositório"
+    commit_msg = "Atualizando arquivo no repositório"
 
     upload_to_github(github_username, github_password, repository_name, local_file_path, commit_msg)
